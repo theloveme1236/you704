ab18b70718a11b78c17faf03476852abacb2915c
theloveme1236 <139608310+theloveme1236@users.noreply.github.com>
2023-10-21 11:22:38 -0700

    Add files via upload

--- tiktok_first.py
+++ tiktok_first.py
@@ -30,7 +30,7 @@ def open_browser():
     options = Options()
     #options.add_argument('--user-data-dir=sesion')
     options.add_argument('--no-sandbox')
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)         
